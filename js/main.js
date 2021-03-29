37e22ab1fe90cc30692ab8cbb4b4e7e1545dced3
Hunter Cogan <robert.h.cogan@gmail.com>
2021-03-29 15:22:33 -0400

    improve nav responsiveness

--- js/main.js
+++ js/main.js
@@ -2,11 +2,22 @@ const $ = function( ele ) {
     return document.querySelector( ele );
 };
 
+// const toggleNav = () => {
+//     let x = $(`#nav-drop`);
+//     if (x.style.display === "flex") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "flex";
+//     }
+// };
+
 const toggleNav = () => {
-    let x = $(`#nav-drop`);
-    if (x.style.display === "flex") {
-        x.style.display = "none";
+    let x = $('#nav-drop');
+    if (x.classList.contains('nav-transition')) {
+        x.classList.add('hidden');
+        x.classList.remove('nav-transition');
     } else {
-        x.style.display = "flex";
+        x.classList.remove('hidden');
+        x.classList.add('nav-transition');
     }
 };
