91cf23d2bd0ade5042e7c420cde3fcfc7cbcf09b
Hunter Cogan <robert.h.cogan@gmail.com>
2021-03-29 16:24:14 -0400

    nav font color adjustment

--- js/main.js
+++ js/main.js
@@ -2,15 +2,6 @@ const $ = ( ele ) => {
     return document.querySelector( ele );
 };
 
-// const toggleNav = () => {
-//     let x = $(`#nav-drop`);
-//     if (x.style.display === "flex") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "flex";
-//     }
-// };
-
 const toggleNav = () => {
     let x = $('#nav-drop');
     if (x.classList.contains('nav-transition')) {
