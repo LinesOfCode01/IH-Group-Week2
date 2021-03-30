0fc8344ca3ec32ecf42482eed58aaf61b4099ad0
Hunter Cogan <robert.h.cogan@gmail.com>
2021-03-30 18:00:16 -0400

    Home page mid-fi and reusable header/footer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ const $ = (e) => {
     return document.querySelector(e);
 };
 
+//Transition Handler for mobile nav
+const toggleNav = () => {
+    let x = $('#mobile-drop');
+    if (x.classList.contains('nav-transition')) {
+        x.classList.add('hidden');
+        x.classList.remove('nav-transition');
+    } else {
+        x.classList.remove('hidden');
+        x.classList.add('nav-transition');
+    }
+};
+
 //Sets current page's nav bar text to bold
 const checkPage = () => document.title.toLowerCase();
 
