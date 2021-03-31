4720107743ac3640aa72562588a6f4aed9e8e119
Hunter Cogan <robert.h.cogan@gmail.com>
2021-03-31 13:16:19 -0400

    close mobile nav menu if screen size changes to desktop dimensions

--- js/main.js
+++ js/main.js
@@ -18,9 +18,12 @@ const toggleNav = () => {
     }
 };
 
+//FOR DEV PURPOSES - Close mobile nav menu if switching to desktop screen size
+window.addEventListener('resize', () =>
+    (window.innerWidth >= 768 || $('#mobile-drop').classList.contains('nav-transition')) ? toggleNav() : false);
+
 //Sets current page's nav bar text to bold
 const checkPage = () => document.title.toLowerCase();
-
 const setAnchorSelected = (page) => {
     let tag = `#${page}`;
     $(tag).style.fontWeight = '600';
