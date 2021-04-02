a36c6066e1e43b8afd3ed9d9ae8aad1d81fb548a
davidmedero <david.gregory.medero@gmail.com>
2021-04-02 09:13:40 -0400

    here we go

--- js/main.js
+++ js/main.js
@@ -31,3 +31,20 @@ const setAnchorSelected = (page) => {
     $(tag).removeAttribute('class');
 };
 setAnchorSelected(checkPage());
+
+function rg(m, n) {
+    return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+function hex(i) {
+    return i.toString(16);
+}
+
+function randColor() {
+    return '#' + hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15)) +
+        hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15));
+}
+
+$(".social").mouseover(function() {
+    $(this).css("background-color",randColor());
+});
