3411e0e5a690989099b180b9444535c58e1e7017
Hunter Cogan <robert.h.cogan@gmail.com>
2021-04-02 12:42:16 -0400

    Remove console.logs

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ const setAnchorSelected = (page) => {
     $(tag).style.fontWeight = '600';
     $(tag).removeAttribute('class');
     let a = document.title.toUpperCase();
-    console.log(a);
     $('.mobile-map').innerHTML = `${a}`;
 };
 setAnchorSelected(checkPage());
